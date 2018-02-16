688494a2b9018ab01d8599b3b6165b6c6ccda850
RadialApps <radialapps@gmail.com>
2018-02-16 23:43:48 +0530

    Fix up demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 
+import { XunkCalendarModule } from './modules/xunk-calendar/xunk-calendar.module';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  public selDate = {};
+
+  ngOnInit() {
+      this.selDate = XunkCalendarModule.getToday();
+  }
+
 }
