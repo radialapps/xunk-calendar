81b7bf9a24b1d879b5955f4982dd6eb0645dcdea
Varun Patil <radialapps@gmail.com>
2018-04-10 22:43:52 +0530

    Deploy to gh-pages with travis

--- deploy.sh
+++ deploy.sh
@@ -1,24 +1,2 @@
 #!/bin/bash
-if [ "$CIRCLE_BRANCH" = "master" ]; then
-
-git config --global user.name "Xunk-Bot"
-git config --global user.email "xunk@radialapps.com"
-git config --global push.default matching
-git clone https://github.com/radialapps/xunk-calendar deploy
-
-cd deploy
-git checkout gh-pages
-rm -r *
-cp -R ../dist/* ./
-cp -R ../.circleci ./
-date > BUILD_TIME
-git add -A
-git commit -m "Automated Build"
-git push
-cd ..
-
-else
-
-echo "Not on master branch"
-
-fi
+echo "Nothing to do!"
